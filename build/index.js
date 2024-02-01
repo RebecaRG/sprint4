0f1686ab76ce91dbf1947cec45ecf07b2693a112
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-02-01 01:37:21 +0100

    Nivell2

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-//Exrecici 1
+//*Exercici 3(NIVELL2)
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,16 +9,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function fetchJoke() {
-    return fetch('https://icanhazdadjoke.com/', {
+//variable contador alternar apis
+let countApi = 0;
+//Get acudits d'icanhazdadjoke
+function fetchDadJoke() {
+    return fetch("https://icanhazdadjoke.com/", {
         headers: {
-            'Accept': 'application/json'
-        }
+            Accept: "application/json",
+        },
     })
         .then((res) => res.json())
         .then((data) => {
         // console.log(data);
         return data;
+    })
+        .catch((err) => {
+        console.error("There are no jokes to show!", err);
+        throw err;
     });
 }
 //Funció per imprimir per pantalla
@@ -30,14 +37,18 @@ function printHtml(joke) {
 }
 //Funció agafa acudit i imprimeix
 function getJoke() {
-    fetchJoke()
-        .then((jokeRandom) => {
-        // console.log(jokeRandom.joke);
-        printHtml(jokeRandom.joke);
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let jokeFetched = yield fetchJoke();
+            printHtml(jokeFetched.joke);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 }
 getJoke();
-//Array per introduir els objectes amb les votacions dels acudits 
+//Array per introduir els objectes amb les votacions dels acudits
 let reportAcudits = [];
 //Guarda la votació
 let puntuacio = 0;
@@ -55,8 +66,9 @@ function getReport(jokeRandom, scoreRandom) {
 }
 //Actualitzar puntuació
 function introReport(puntuacio) {
-    return fetchJoke().then((fJoke) => {
-        let newReport = getReport(fJoke, puntuacio);
+    return __awaiter(this, void 0, void 0, function* () {
+        let fetcthJ = yield fetchJoke();
+        let newReport = getReport(fetcthJ, puntuacio);
         // console.log(newReport);
         return newReport;
     });
@@ -78,5 +90,57 @@ function next() {
         reportAcudits.push(lastScore);
         lastScore = null;
         console.table(reportAcudits);
+    }
+}
+//NIVELL2
+//Exercici4
+//Imprimir temperatura i html a Html
+let tempPrintHtml = document.getElementById("tempPrint");
+let iconPrintHtml = document.getElementById("iconPrint");
+//Funció que llegeig API i imprimir per pantalla
+function getWeather() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?id=3128760&appid=23ebbc592b1e6b4598e259f981fa0834")
+        .then((response) => response.json())
+        .then((data) => {
+        let temperature = data.main.temp;
+        let tempCelsius = (temperature - 273.15).toFixed(1);
+        let icon = data.weather[0].icon;
+        let iconHtml = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">`;
+        if (tempPrintHtml) {
+            tempPrintHtml.innerHTML = tempCelsius + "ºC";
+        }
+        if (iconPrintHtml) {
+            iconPrintHtml.innerHTML = iconHtml;
+        }
+        // console.log(`Temperature: ${tempCelsius}`);
+        // console.log(`Icon: ${iconHtml}`);
+    });
+}
+getWeather();
+//Get acudits Chuck Norris
+function fetchChuckJoke() {
+    return fetch("https://api.chucknorris.io/jokes/random")
+        .then((response) => response.json())
+        .then((data) => {
+        let jokeData = {
+            joke: data.value,
+            id: data.id,
+        };
+        // console.log(jokeData);
+        return jokeData;
+    })
+        .catch((err) => {
+        console.error("There are no jokes to show!", err);
+        throw err;
+    });
+}
+function fetchJoke() {
+    if (countApi === 0) {
+        countApi = 1;
+        return fetchDadJoke();
+    }
+    else {
+        countApi = 0;
+        return fetchChuckJoke();
     }
 }
--- index.ts
+++ index.ts
@@ -1,99 +1,180 @@
+//*Exercici 3(NIVELL2)
+
+//variable contador alternar apis
+let countApi: number = 0;
+
+// -------------------- //
+
+//NIVELL1
 //Exrecici 1
 
 interface Joke {
     id: string;
     joke: string;
-    status: number;
 }
 
-function fetchJoke(): Promise<Joke>{
-    return fetch('https://icanhazdadjoke.com/',{
-    headers: {
-        'Accept': 'application/json'
-    }
-})
-    .then((res) => res.json())
-    .then((data: Joke) =>{
-        // console.log(data);
-        return data
-    });
+//Get acudits d'icanhazdadjoke
+function fetchDadJoke(): Promise<Joke> {
+    return fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data: Joke) => {
+            // console.log(data);
+            return data;
+        })
+        .catch((err: Error) => {
+            console.error("There are no jokes to show!", err);
+            throw err;
+        });
 }
 
 //Funció per imprimir per pantalla
-function printHtml(joke: string):void{
-    let jokePrintHtml = document.getElementById("jokePrint");
-    if(jokePrintHtml){
+function printHtml(joke: string): void {
+    let jokePrintHtml: HTMLElement | null = document.getElementById("jokePrint");
+    if (jokePrintHtml) {
         jokePrintHtml.innerHTML = joke;
     }
 }
 
 //Funció agafa acudit i imprimeix
-function getJoke():void{
-    fetchJoke()
-        .then((jokeRandom: Joke)=> {
-        // console.log(jokeRandom.joke);
-        printHtml(jokeRandom.joke);
-    });
+async function getJoke(): Promise<void> {
+    try {
+        let jokeFetched: Joke = await fetchJoke();
+        printHtml(jokeFetched.joke);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 getJoke();
 
-
 // Exercici 3
-
-interface infoReportAcudits{
+interface infoReportAcudits {
     joke: string;
     score: number;
     date: string;
-} 
+}
 
-//Array per introduir els objectes amb les votacions dels acudits 
+//Array per introduir els objectes amb les votacions dels acudits
 let reportAcudits: infoReportAcudits[] = [];
 
 //Guarda la votació
-let puntuacio:number = 0;
+let puntuacio: number = 0;
 
 //Guarda objecte amb última puntuació introduida
-let lastScore: infoReportAcudits|null;
+let lastScore: infoReportAcudits | null;
 
 //Crear objecte amb la puntuació
-function getReport(jokeRandom: Joke, scoreRandom:number){
+function getReport(jokeRandom: Joke, scoreRandom: number): infoReportAcudits {
     let newReport: infoReportAcudits = {
         joke: jokeRandom.id,
         score: scoreRandom,
         date: new Date().toISOString(),
-    }
-    
+    };
+
     // console.log(newReport);
     return newReport;
 }
 
 //Actualitzar puntuació
-function introReport(puntuacio: number): Promise<infoReportAcudits> {
-    return fetchJoke().then((fJoke) => {
-        let newReport = getReport(fJoke, puntuacio);
-        // console.log(newReport);
-        return newReport;
-    });
+async function introReport(puntuacio: number): Promise<infoReportAcudits> {
+    let fetcthJ: Joke = await fetchJoke();
+    let newReport: infoReportAcudits = getReport(fetcthJ, puntuacio);
+    // console.log(newReport);
+    return newReport;
 }
 
 introReport(puntuacio);
 
-
 //Funció per saber puntuació
-async function punts(score:number): Promise<infoReportAcudits> {
+async function punts(score: number): Promise<infoReportAcudits> {
     puntuacio = score;
-    let newScore = await introReport(score);
+    let newScore: infoReportAcudits = await introReport(score);
     lastScore = newScore;
     return newScore;
 }
 
 //Funció següent acudit(Exercici 1 i 2)
-function next(){
+function next(): void {
     getJoke();
-    if(lastScore != null){
-    reportAcudits.push(lastScore);
-    lastScore = null;
-    console.table(reportAcudits);
+    if (lastScore != null) {
+        reportAcudits.push(lastScore);
+        lastScore = null;
+        console.table(reportAcudits);
+    }
 }
+
+//NIVELL2
+//Exercici4
+//Imprimir temperatura i html a Html
+let tempPrintHtml: HTMLElement | null = document.getElementById("tempPrint");
+let iconPrintHtml: HTMLElement | null = document.getElementById("iconPrint");
+interface WeatherData {
+    main: {
+        temp: number;
+    };
+    weather: Array<{
+        icon: string;
+    }>;
+}
+
+//Funció que llegeig API i imprimir per pantalla
+function getWeather(): void {
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?id=3128760&appid=23ebbc592b1e6b4598e259f981fa0834"
+    )
+        .then((response) => response.json())
+        .then((data: WeatherData) => {
+            let temperature: number = data.main.temp;
+            let tempCelsius: string = (temperature - 273.15).toFixed(1);
+            let icon: string = data.weather[0].icon;
+            let iconHtml: string = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">`;
+            if (tempPrintHtml) {
+                tempPrintHtml.innerHTML = tempCelsius + "ºC";
+            }
+            if (iconPrintHtml) {
+                iconPrintHtml.innerHTML = iconHtml;
+            }
+            // console.log(`Temperature: ${tempCelsius}`);
+            // console.log(`Icon: ${iconHtml}`);
+        });
+}
+
+getWeather();
+
+//Exercici5
+interface JokeChuck {
+    id: string;
+    value: string;
+}
+
+//Get acudits Chuck Norris
+function fetchChuckJoke(): Promise<Joke> {
+    return fetch("https://api.chucknorris.io/jokes/random")
+        .then((response) => response.json())
+        .then((data) => {
+            let jokeData: Joke = {
+                joke: data.value,
+                id: data.id,
+            };
+            // console.log(jokeData);
+            return jokeData;
+        })
+        .catch((err: Error) => {
+            console.error("There are no jokes to show!", err);
+            throw err;
+        });
+}
+
+function fetchJoke(): Promise<Joke> {
+    if (countApi === 0) {
+        countApi = 1;
+        return fetchDadJoke();
+    } else {
+        countApi = 0;
+        return fetchChuckJoke();
+    }
 }
